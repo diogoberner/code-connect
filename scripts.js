570463ed07e257eaea444ef583e7efc3b01d57f9
Diogo CS Berner <diogoberner@gmail.com>
2025-01-24 19:02:29 -0300

    tags started

--- scripts.js
+++ scripts.js
@@ -36,3 +36,31 @@ inputUpload.addEventListener("change", async (e) => {
         }
     }
 })
+
+const tagsInput = document.querySelector("#category")
+
+tagsInput.addEventListener("keypress", (e) => {
+    
+    if (e.key === "Enter"){
+        e.preventDefault()
+        
+        const tagText = tagsInput.value.trim()
+
+        if (tagText !== "") {
+            const tagsList = document.querySelector(".list-tags")
+            const newTag = document.createElement("li")
+            const tagName = document.createElement("p")
+            tagName.textContent = e.target.value
+            const tagImage = document.createElement("img")
+            tagImage.src = "./img/close-black.svg"
+            newTag.appendChild(tagName)
+            newTag.appendChild(tagImage)
+            tagsList.appendChild(newTag)
+    
+            tagsInput.value = ""
+        }
+
+    }
+    
+
+})
